e480bc56979dd498b74465069c7ea55fc5fffa61
Mohamed Hanafy <mohamed.hanfy.dev@outlook.com>
2023-09-12 19:57:51 +0300

    update main file

--- library system/library system.cpp
+++ library system/library system.cpp
@@ -6,51 +6,51 @@
 #include"UserList.h"
 #include"BookList.h"
 void main_menu() {
-    cout << "Select one of the following choices : " << '\n';
-    cout << "1- Books Menu" << '\n';
-    cout << "2- Users Menu" << '\n';
-    cout << "3- Exit " << '\n';
+    cout << "Select one of the following choices : " << endl;
+    cout << "1- Books Menu" << endl;
+    cout << "2- Users Menu" << endl;
+    cout << "3- Exit " << endl;
 }
 void user_menu() {
-    cout << " USERS MENU" << '\n';
-    cout << "1- Create a USER and add it to the list " << '\n';
-    cout << "2- Search for a user " << '\n';
-    cout << "3- Display all users " << '\n';
-    cout << "4- Back to the main " << '\n';
+    cout << " USERS MENU" << endl;
+    cout << "1- Create a USER and add it to the list " << endl;
+    cout << "2- Search for a user " << endl;
+    cout << "3- Display all users " << endl;
+    cout << "4- Back to the main " << endl;
 }
 void search_user_menu() {
-    cout << "SEARCH FOR A USER" << '\n';
-    cout << "1- Search by name" << '\n';
-    cout << "2- Search by id" << '\n';
-    cout << "3- Return to users Menu" << '\n';
+    cout << "SEARCH FOR A USER" << endl;
+    cout << "1- Search by name" << endl;
+    cout << "2- Search by id" << endl;
+    cout << "3- Return to users Menu" << endl;
 }
 void delete_user_menu() {
-    cout << "1- Delete user " << '\n';
-    cout << "2- Return to users Menu" << '\n';
+    cout << "1- Delete user " << endl;
+    cout << "2- Return to users Menu" << endl;
 }
 void book_menu() {
-    cout << "BOOKS MENU" << '\n';
-    cout << "1- Create a book and add it to the list " << '\n';
-    cout << "2- Search for a book" << '\n';
-    cout << "3- Display all books (with book rating)" << '\n';
-    cout << "4- Get the highest rating" << '\n';
-    cout << "5- Get all books of a user " << '\n';
-    cout << "6- Back to the main menu" << '\n';
+    cout << "BOOKS MENU" << endl;
+    cout << "1- Create a book and add it to the list " << endl;
+    cout << "2- Search for a book" << endl;
+    cout << "3- Display all books (with book rating)" << endl;
+    cout << "4- Get the highest rating" << endl;
+    cout << "5- Get all books of a user " << endl;
+    cout << "6- Back to the main menu" << endl;
 }
 void search_book_menu() {
-    cout << "SEARCH FOR A BOOK" << '\n';
-    cout << "1- Search by name" << '\n';
-    cout << "2- Search by id" << '\n';
-    cout << "3- Return to Books Menu" << '\n';
+    cout << "SEARCH FOR A BOOK" << endl;
+    cout << "1- Search by name" << endl;
+    cout << "2- Search by id" << endl;
+    cout << "3- Return to Books Menu" << endl;
 }
 void search_book_name()
 {
-    cout << "1- Update author" << '\n';
-    cout << "2- Update name" << '\n';
-    cout << "3- Update Category" << '\n';
-    cout << "4- Delete Book" << '\n';
-    cout << "5- rate book" << '\n';
-    cout << "6- Get back to books menu" << '\n';
+    cout << "1- Update author" << endl;
+    cout << "2- Update name" << endl;
+    cout << "3- Update Category" << endl;
+    cout << "4- Delete Book" << endl;
+    cout << "5- rate book" <<endl;
+    cout << "6- Get back to books menu" << endl;
 }
 int main()
 {
